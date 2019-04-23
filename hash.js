b2d1feaeed3a8ad3d56b9f38ed7b290072775cb7
Kenny Whyte <kawhyte@gmail.com>
2019-04-23 14:43:45 -0700

    added Hash

---
+++ hash.js
@@ -0,0 +1,12 @@
+const bcrypt  = require('bcryptjs');
+
+
+
+async function run(){
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash('1234',salt);
+    console.log(salt);
+    console.log(hash);
+}
+
+run();
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
+const bcrypt = require('bcryptjs');
 const _ = require('lodash');
-
 const {User,validate} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -14,7 +14,8 @@ router.post('/', async (req, res) => {
     if (user) return res.status(400).send('User already exsist.');
 
     user = new User ( _.pick(req.body, ['name', 'email','password']));
-
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password,salt);
     await user.save();
 
     res.send( _.pick(user,['_id','name', 'email'])); 
