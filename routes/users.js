aad51ba85026f5407ff0c88ca76846b3f26e3b47
Kenny Whyte <kawhyte@gmail.com>
2019-04-23 13:04:51 -0700

    added lodash

--- routes/users.js
+++ routes/users.js
@@ -13,18 +13,11 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({email: req.body.email});
     if (user) return res.status(400).send('User already exsist.');
 
-    user = new User ({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
+    user = new User ( _.pick(req.body, ['name', 'email','password']));
 
     await user.save();
 
-    
-
-
-    res.send( _.pick(user,['_id','name', 'email']); 
+    res.send( _.pick(user,['_id','name', 'email'])); 
 });
 
 module.exports = router;
