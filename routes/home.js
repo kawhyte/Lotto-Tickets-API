c8ef2cd6c2120f98ca08a3b46a8c8d066a89bd75
Kenny Whyte <kawhyte@gmail.com>
2019-05-03 20:23:15 -0700

    Hello world prod test

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //HOME PAGE
 router.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello from the other side!');
 });
 
 module.exports = router;
