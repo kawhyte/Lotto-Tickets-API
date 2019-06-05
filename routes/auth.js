0015c13a37064165bd3301815e0c3ebf44159f57
Kenny <kawhyte@gmail.com>
2019-06-05 09:55:41 -0700

    Prod Test

--- routes/auth.js
+++ routes/auth.js
@@ -15,11 +15,11 @@ router.post('/', async (req, res) => {
     if (error) return res.status(400).send(error.details[0].message);
 
     let user = await User.findOne({email: req.body.email});
-    if (!user) return res.status(400).send('Invalid Credentials.');
+    if (!user) return res.status(400).send('Email-Invalid Credentials.');
 
     validPassword = await bcrypt.compare(req.body.password, user.password)
 
-    if (!validPassword) return res.status(400).send('Invalid Credentials.');
+    if (!validPassword) return res.status(400).send('Password - Invalid Credentials.');
     
     const token = user.generateAuthToken();
     res.send(token); 
